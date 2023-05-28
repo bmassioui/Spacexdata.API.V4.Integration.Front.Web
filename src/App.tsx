20e5b0698c5942638f63b8a810e6d9f8ad70dbcc
bmassioui <bouchaib.massioui@gmail.com>
2023-05-28 04:49:00 +0100

    :art: use free version of BloomUI.com

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { useRoutes } from 'react-router-dom';
+import router from 'src/router';
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+
+import { CssBaseline } from '@mui/material';
+import ThemeProvider from './theme/ThemeProvider';
 
 function App() {
-  return (
-    <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </div>
-  )
-}
+  const content = useRoutes(router);
 
-export default App
+  return (
+    <ThemeProvider>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <CssBaseline />
+        {content}
+      </LocalizationProvider>
+    </ThemeProvider>
+  );
+}
+export default App;
